9db8c4048dc34018894aaaa4b72ea0b0a86d3afc
Átila Camurça <camurca.home@gmail.com>
2015-07-20 19:38:06 -0300

    Initial commit.

---
+++ arduino-drum-fofix.ino
@@ -0,0 +1,77 @@
+/**
+ * Arduino Drum for FoFiX
+ */
+
+#define NUM_SENSORS 6
+#define DRUM_HIT 1000      // Minimum Analog value to cause a drum hit
+#define DELAY 100
+#define MAX_PLAY_TIME 100  // Cycles before a 2nd hit is allowed
+
+// these constants won't change:
+const int ledPin = 13;      // led connected to digital pin 13
+const int threshold = 100;  // threshold value to decide when the detected sound is a knock or not
+
+const int knocks[NUM_SENSORS] = {A0, A1, A2, A3, A4, A5}; // the piezos are connected to analog pins
+
+boolean activePad[NUM_SENSORS] = {0,0,0,0,0,0};                   // Array of flags of pad currently playing
+int PinPlayTime[NUM_SENSORS] = {0,0,0,0,0,0};                     // Counter since pad started to play
+
+int sensorReading = 0;      // variable to store the value read from the sensor pin
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(ledPin, OUTPUT); // declare the ledPin as as OUTPUT
+  Serial.begin(9600);       // use the serial port
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  for (int i = 0; i < NUM_SENSORS; i++) {
+    
+    sensorReading = analogRead(knocks[i]);
+    
+    if((sensorReading > DRUM_HIT)) {
+
+      if((activePad[i] == false)) {
+        play(knocks[i], sensorReading);
+        PinPlayTime[i] = 0;
+        activePad[i] = true;
+      } else {
+        PinPlayTime[i] = PinPlayTime[i] + 1;
+      }
+      
+    } else if((activePad[i] == true)) {
+      PinPlayTime[i] = PinPlayTime[i] + 1;
+      if(PinPlayTime[i] > MAX_PLAY_TIME) {
+        activePad[i] = false;
+        play(knocks[i], sensorReading);
+      }
+    }
+  }
+}
+
+void play(int knock, int sensorReading) {
+  if (sensorReading > DRUM_HIT) {
+    Serial.print("Sensor reading: ");
+    Serial.print(sensorReading);
+    switch(knock) {
+      case A0:
+        Serial.println(", Knock Red!");
+        break;
+      case A1:
+        Serial.println(", Knock Yellow!");
+        break;
+      case A2:
+        Serial.println(", Knock Blue!");
+        break;
+      case A3:
+        Serial.println(", Knock Green!");
+        break;
+      case A4:
+        Serial.println(", Knock Orange!");
+        break;
+    }
+    delay(DELAY);
+  }
+}
+
